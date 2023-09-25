b8ae7240a252b75429ae71e131b822ebdee011e8
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-09-25 22:42:32 +0530

    changes

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '@/components/Hero'
+import Header from '@/components/Header'
+const page = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero />
+    </div>
+  )
+}
+
+export default page
---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-center">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
