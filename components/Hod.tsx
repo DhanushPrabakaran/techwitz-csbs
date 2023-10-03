5f1c7cc3cd9001d9ff2551d4c43fb51352353ed3
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-03 13:09:12 +0530

    changes

--- components/Hod.tsx
+++ components/Hod.tsx
@@ -15,7 +15,7 @@ const Hod = () => {
       <div className="hero-content text-center text-neutral-content">
         <div className=" flex flex-col justify-center items-center">
           <h1 className="mb-5 text-5xl font-bold uppercase inline-flex text-secondary animate-[fade-in-down_2s_ease-in-out]">
-            Dean Placement
+           Head Of The Department
           </h1>
           <Image
             className="mask mask-squircle animate-[fade-in_2s_ease-in-out]"
@@ -24,25 +24,45 @@ const Hod = () => {
             src={Dean_Mam}
             alt="picture"
           />
+          {/* <h1 className="text-5xl font-bold animate-[fade-in-down_2s_ease-in-out] text-secondary">
+            Chief Guest{" "}
+          </h1> */}
+          <h2 className=" font-bold m-2 text-3xl text-primary animate-[fade-in-left_2s_ease-in-out] ">
+          Dr.K.Mahalakshmi
+          </h2>
+          <h2 className=" font-bold text-2xl m-2  text-secondary animate-[fade-in-left_2s_ease-in-out] ">
+          Dean Placements,HOD-CSBS
+          </h2>
+          <div className="animate-[fade-in-left_2s_ease-in-out] text-xl m-2 ">
+          KIT - KALAIGNARKARUNANIDHI INSTITUTE OF TECHNOLOGY
+          </div>
+          {/* <p className="py-6 animate-[fade-in-right_2s_ease-in-out] text-center font-mono">
+            <span className="bg-secondary rounded-md text-black px-1 ">
+              {" "}
+              Project Management professional
+            </span>{" "}
+            |{" "}
+            <span className="bg-secondary rounded-md text-black px-1 ">
+              {" "}
+              Space and Aerospace domain{" "}
+            </span>{" "}
+            |{" "}
+            <span className="bg-secondary rounded-md text-black px-1 inline-block ">
+              Strategic, Data driven and logical thinker
+            </span>{" "}
+            |{" "}
+            <span className="bg-secondary rounded-md text-black px-1 ">
+              {" "}
+              Continuous Learner
+            </span>{" "}
+          </p> */}
           <p className="mb-5 animate-[fade-in-left_2s_ease-in-out] text-clip">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          Experienced Professor with a demonstrated history of working in the education management industry. I love encouraging people and I want to reach a grater community. I am no stranger to hard work and sacrifice, I have already reached out and helped individuals/students create a blueprint for success in their lives through my care and motivation.
           </p>
           <div className="flex justify-center">
-            <a className="m-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="40"
-                height="40"
-                id="instagram"
-                className="fill-current hover:fill-secondary"
-              >
-                <path d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"></path>
-              </svg>
-            </a>
-            <a className="m-2">
+          <h2 className='text-secondary font-bold' >Profile: </h2>
+            <a className="m-2"
+            href="https://www.linkedin.com/in/dr-mahalakshmi-k-00068b65/?originalSubdomain=in" target="_blank">
               <svg
                 height="40"
                 width="40"
@@ -57,23 +77,12 @@ const Hod = () => {
                   />
                   <path
                     d="M77.796 214.238V98.986H39.488v115.252H77.8zM58.65 83.253c13.356 0 21.671-8.85 21.671-19.91-.25-11.312-8.315-19.915-21.417-19.915-13.111 0-21.674 8.603-21.674 19.914 0 11.06 8.312 19.91 21.169 19.91h.248zM99 214.238h38.305v-64.355c0-3.44.25-6.889 1.262-9.346 2.768-6.885 9.071-14.012 19.656-14.012 13.858 0 19.405 10.568 19.405 26.063v61.65h38.304v-66.082c0-35.399-18.896-51.872-44.099-51.872-20.663 0-29.738 11.549-34.78 19.415h.255V98.99H99.002c.5 10.812-.003 115.252-.003 115.252z"
-                   
                     className="fill-current hover:fill-secondary"
                   />
                 </g>
               </svg>
             </a>
-            <a className="m-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 24 24"
-                className="fill-current hover:fill-secondary"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
+            
           </div>
         </div>
       </div>
